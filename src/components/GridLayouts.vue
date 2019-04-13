70268230f36f6e2c027f843fe32f9ff58115085a
Ysmaelov <ysmaelov@gmail.com>
2019-04-13 19:44:43 +0200

    Añadir componentes a grid draggable

--- src/components/GridLayouts.vue
+++ src/components/GridLayouts.vue
@@ -18,7 +18,8 @@
                :w="item.w"
                :h="item.h"
                :i="item.i">
-      {{item.i}}
+      <!--<span>{{item.i}}</span>-->
+      <component :is="item.is"></component>
     </grid-item>
   </grid-layout>
 </template>
@@ -27,10 +28,12 @@
   import { GridLayout } from 'vue-grid-layout';
   import { GridItem } from 'vue-grid-layout';
   import 'vue-grid-layout/public/app.css'
+  import HelloWorld from "./HelloWorld";
+  import MenuSidebar from './MenuSidebar'
   let testLayout = [
-    {"x":0,"y":0,"w":2,"h":2,"i":"0"},
-    {"x":2,"y":0,"w":2,"h":4,"i":"1"},
-    {"x":4,"y":0,"w":2,"h":5,"i":"2"},
+    {is:'HelloWorld',"x":0,"y":0,"w":2,"h":2,"i":"0"},
+    {is:'MenuSidebar',"x":2,"y":0,"w":2,"h":4,"i":"1"},
+   /* {"x":4,"y":0,"w":2,"h":5,"i":"2"},
     {"x":6,"y":0,"w":2,"h":3,"i":"3"},
     {"x":8,"y":0,"w":2,"h":3,"i":"4"},
     {"x":10,"y":0,"w":2,"h":3,"i":"5"},
@@ -47,11 +50,13 @@
     {"x":8,"y":10,"w":2,"h":5,"i":"16"},
     {"x":10,"y":4,"w":2,"h":2,"i":"17"},
     {"x":0,"y":9,"w":2,"h":3,"i":"18"},
-    {"x":2,"y":6,"w":2,"h":2,"i":"19"}
+    {"x":2,"y":6,"w":2,"h":2,"i":"19"}*/
   ];
   export default {
     components: {
+      HelloWorld,
       GridLayout,
+      MenuSidebar,
       GridItem
     },
     data(){
@@ -68,5 +73,5 @@
 </script>
 
 <style scoped>
-
+  .vue-grid-item{overflow: hidden}
 </style>
